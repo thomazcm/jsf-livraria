30c1fea72779ef8890d5b6ee3f044c148f04f942
Thomaz Machado <thomazcm@gmail.com>
2023-06-10 16:51:57 -0300

    initial commit, simple JSF form

---
+++ src/main/java/com/thomazcm/livraria/bean/LivroBean.java
@@ -0,0 +1,12 @@
+package com.thomazcm.livraria.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LivroBean {
+
+    public void gravar() {
+        System.out.println("Gravando livro...");
+    }
+    
+}
