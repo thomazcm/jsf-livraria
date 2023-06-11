6966930e96827bf5feff76f7ae309897b565691b
Thomaz Machado <thomazcm@gmail.com>
2023-06-11 16:58:16 -0300

    Restructuring project to work with MySQL 5.7

--- src/main/java/com/thomazcm/livraria/dao/JPAUtil.java
+++ src/main/java/com/thomazcm/livraria/dao/JPAUtil.java
@@ -7,7 +7,7 @@ import javax.persistence.Persistence;
 public class JPAUtil {
 
     private static EntityManagerFactory emf = Persistence
-            .createEntityManagerFactory("livraria");
+            .createEntityManagerFactory("jsflivraria");
 
     public EntityManager getEntityManager() {
         return emf.createEntityManager();
